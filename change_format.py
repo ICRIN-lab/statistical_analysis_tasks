862c86f54d17558e609ced12da97018d9fd4d814
Melissa Marius <melissamarius@170.immuno.chups.jussieu.fr>
2022-05-13 11:00:41 +0200

    first commit

---
+++ change_format.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def correct_format(df):  # changer correct en result
+    # df['correct'] = np.where(df['correct'] == True, 1, 0)
+    df.rename(columns={'correct': 'result'}, inplace=True)
+    return df
